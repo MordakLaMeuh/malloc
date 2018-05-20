5e2c34bef87027375da90108f9d47e6ad87622af
Billod MICKAEL <bmickael@e3r3p7.42.fr>
2018-05-20 02:45:34 +0200

    mini refactoring dossier

--- include/libft_alloc.h
+++ include/libft_alloc.h
@@ -17,11 +17,12 @@
 
 void	free(void *ptr);
 void	*malloc(size_t size);
-void	*realloc(void *ptr, size_t size);
-void	*reallocarray(void *ptr, size_t nmemb, size_t size);
 void	*calloc(size_t count, size_t size);
-void	show_alloc_mem(void);
-void	*valloc(size_t size);
+void	*realloc(void *ptr, size_t size);
 void	*reallocf(void *ptr, size_t size);
+void	*reallocarray(void *ptr, size_t nmemb, size_t size);
+void	*valloc(size_t size);
+void	show_alloc_mem(void);
+void	show_alloc_mem_ex(void);
 
 #endif
